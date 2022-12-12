ea48134fbe4b63c98b097e0394201e7694fa0465
BrunoZarth <brunozarth@gmail.com>
2022-12-12 15:14:26 -0300

    implements exception

--- src/main/java/com/brunozarth/equipmentapi/service/IClientService.java
+++ src/main/java/com/brunozarth/equipmentapi/service/IClientService.java
@@ -1,4 +1,25 @@
 package com.brunozarth.equipmentapi.service;
 
+import com.brunozarth.equipmentapi.entity.Client;
+import com.brunozarth.equipmentapi.entity.form.ClientForm;
+
+import java.util.List;
+
 public interface IClientService {
+
+    //GET
+    List<Client> findAll();
+
+    List<Client> findByName(String name);
+
+    Client findById(Long id);
+
+    //POST
+    Client saveClient(ClientForm clientForm);
+
+    //UPDATE
+    Client updateClient(Long id, ClientForm clientForm);
+
+    //DELETE
+    void deleteClient(Long id);
 }
--- src/main/java/com/brunozarth/equipmentapi/service/impl/ClientServiceImpl.java
+++ src/main/java/com/brunozarth/equipmentapi/service/impl/ClientServiceImpl.java
@@ -1,4 +1,47 @@
 package com.brunozarth.equipmentapi.service.impl;
 
-public class ClientServiceImpl {
+import com.brunozarth.equipmentapi.entity.Client;
+import com.brunozarth.equipmentapi.entity.form.ClientForm;
+import com.brunozarth.equipmentapi.exception.BadRequestException;
+import com.brunozarth.equipmentapi.repository.ClientRepository;
+import com.brunozarth.equipmentapi.service.IClientService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ClientServiceImpl implements IClientService {
+
+    ClientRepository clientRepository;
+    public ClientServiceImpl(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+    @Override
+    public List<Client> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Client> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Client findById(Long id) {
+        return clientRepository.findById(id).orElseThrow(() -> new BadRequestException("Equipment id not found!"));
+    }
+
+    @Override
+    public Client saveClient(ClientForm clientForm) {
+        return null;
+    }
+
+    @Override
+    public Client updateClient(Long id, ClientForm clientForm) {
+        return null;
+    }
+
+    @Override
+    public void deleteClient(Long id) {
+
+    }
 }
